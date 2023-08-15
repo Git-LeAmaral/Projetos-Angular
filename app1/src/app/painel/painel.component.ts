24086209694b7606840958a1397c44879652e048
Leandro Amaral <lramaral02m@gmail.com>
2023-08-15 16:15:52 -0300

    Estabelecendo a lógica das rodadas de frases

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -13,13 +13,37 @@ export class PainelComponent {
   public instrucao: string = 'Traduza a frase:'
   public resposta: string | undefined
 
-  constructor() {console.log(this.frases);
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase);
   }
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
 
-    console.log(this.resposta);
+    // console.log(this.resposta);
+
+  }
+
+  public verificarResposta(): void {
+
+    if( this.rodadaFrase.frasePtBr == this.resposta ) {
+      alert('A tradução está correta')
+
+          //trocar pergunta da rodada
+        this.rodada++
+
+          //atualiza o objeto rodadafrase
+        console.log(this.rodada);
+        this.rodadaFrase = this.frases[this.rodada]
+        console.log(this.rodadaFrase);
+
+    } else {
+      alert('A tradução está errada')
+    }
 
   }
 
